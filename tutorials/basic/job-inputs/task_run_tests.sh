1a5381f2dac659e9f5daa51100165384c7bf93ae
Hisham Hassan PM <34262692+hishamhpm@users.noreply.github.com>
2019-01-27 23:23:45 +0000

    Reverted changes

--- tutorials/basic/job-inputs/task_run_tests.sh
+++ tutorials/basic/job-inputs/task_run_tests.sh
@@ -5,10 +5,7 @@ set -e
 # task script is in resource-tutorial/job-inputs/ folder
 # application input is in gopath/src/github.com/cloudfoundry-community/simple-go-web-app folder
 # $GOPATH is gopath/ folder
-
 export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/cloudfoundry-community/simple-go-web-app/Godeps/_workspace
 cd gopath/src/github.com/cloudfoundry-community/simple-go-web-app/
-
-echo $GOPATH
 
 go test ./...
